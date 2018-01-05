85c8677de923e69cb009a0bf217b5bc4a15d36ae
w可 <w@DESKTOP-ARHOPSG>
2018-01-05 21:26:02 +0800

    start

---
+++ src/test/java/com/wza/maven/pojo/po/StudentTest.java
@@ -0,0 +1,12 @@
+package com.wza.maven.pojo.po;
+
+import org.junit.Test;
+
+public class StudentTest {
+	/*@Test
+	public void test(){
+		Student stu = new Student();
+		stu.setId("231");
+		System.out.println(stu);
+	}*/
+}
---
+++ src/test/java/com/wza/maven/pojo/po/UserTest.java
@@ -0,0 +1,12 @@
+package com.wza.maven.pojo.po;
+
+import org.junit.Test;
+
+public class UserTest {
+	@Test
+	public void userTest(){
+		User user = new User();
+		System.out.println(user.getName()+" "+user.getPassword());
+		
+	}
+}
